d63aea5ae873216b317ccab83f8b19b517cbfe73
Robert Schumacher <roschuma@microsoft.com>
2014-09-24 01:42:46 +0000

    Added dev14 DevToolsVersion and powershell script

---
+++ setup_ps_env_VS2014.ps1
@@ -0,0 +1,22 @@
+function Get-Batchfile ($file) {
+    $cmd = "@echo off & `"$file`" & set"
+    cmd /c $cmd | Foreach-Object {
+        $p, $v = $_.split('=')
+        Set-Item -path env:$p -value $v
+    }
+}
+
+function VsVars32()
+{
+    $vs140comntools = (Get-ChildItem env:VS140COMNTOOLS).Value
+    $batchFile = [System.IO.Path]::Combine($vs140comntools, "vsvars32.bat")
+    Get-Batchfile $BatchFile
+}
+
+"Initializing Casablanca Powershell VS2014 Environment"
+
+# get VS tools
+VsVars32
+
+$Env:VisualStudioVersion = "14.0"
+$Env:DevToolsVersion = "140"
