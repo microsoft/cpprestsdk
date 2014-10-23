d3e48ebaed54ae67b5b03960dbe3dd61df104ada
Robert Schumacher <roschuma@microsoft.com>
2014-10-23 11:19:08 -0700

    Modified configure.sh script to add command line arguments and update the version of Boost-for-Android, check for out-of-source build, added license stub, and changed cmake configure line to use clang3.4

--- Build_android/configure.sh
+++ Build_android/configure.sh
@@ -1,17 +1,88 @@
 #!/bin/bash
+# ==++==
+#
+# Copyright (c) Microsoft Corporation. All rights reserved. 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ==--==
+# =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+#
+# configure.sh
+#
+# Build script for casablanca on android
+#
+# For the latest on this and related APIs, please see http://casablanca.codeplex.com.
+#
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
 set -e
 
 # Note: we require android ndk r9d available from
 # https://dl.google.com/android/ndk/android-ndk-r9d-linux-x86_64.tar.bz2
 # https://dl.google.com/android/ndk/android-ndk-r9d-windows-x86_64.zip
 
+# -----------------
+# Parse args
+# -----------------
+
+DO_LIBICONV=1
+DO_BOOST=1
+DO_OPENSSL=1
+
+function usage {
+    echo "Usage: $0 [--skip-boost] [--skip-openssl] [-h] [--ndk <android-ndk>]"
+    echo ""
+    echo "    --skip-boost          Skip fetching and compiling boost"
+    echo "    --skip-openssl        Skip fetching and compiling openssl"
+    echo "    --skip-libiconv       Skip fetching and compiling libiconv"
+    echo "    -h,--help,-?          Display this information"
+    echo "    --ndk <android-ndk>   If specified, overrides the ANDROID_NDK environment variable"
+}
+
+while [[ $# > 0 ]]
+do
+    case $1 in
+	"--skip-boost")
+	    DO_BOOST=0
+	    ;;
+	"--skip-openssl")
+	    DO_OPENSSL=0
+	    ;;
+	"--skip-libiconv")
+	    DO_LIBICONV=0
+	    ;;
+	"-?"|"-h"|"--help")
+	    usage
+	    exit
+	    ;;
+	"--ndk")
+	    shift
+	    export ANDROID_NDK=$1
+	    ;;
+	*)
+	    usage
+	    exit 1
+	    ;;
+    esac
+    shift
+done
+
 # Variables setup
 
-if [ ! -e "${ANDROID_NDK}" ]
+if [ ! -e "${ANDROID_NDK}/ndk-build" ]
 then
     echo "ANDROID_NDK does not point to a valid NDK."
     echo "(current setting: '${ANDROID_NDK}')"
-    exit
+    exit 1
 fi
 NDK_DIR=`cd "${ANDROID_NDK}" && pwd`
 SRC_DIR=`pwd`
@@ -31,13 +102,23 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
+if [ "$SRC_DIR" == "$DIR" ]
+then
+    echo "You must use a separate directory for building."
+    echo "(try 'mkdir build; cd build; ../configure.sh')"
+    exit 1
+fi
+
 # -------
 # Openssl
 # -------
 
 # This steps are based on the github project openssl1.0.1g-android
 # https://github.com/aluvalasuman/openssl1.0.1g-android
+if [ "${DO_OPENSSL}" == "1" ]
+then
 (
+    rm -rf openssl
     mkdir openssl
     cd openssl
     if [ ! -e "openssl-1.0.1h.tar.gz" ]
@@ -49,9 +130,10 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
     cd openssl-1.0.1h
     export ANDROID_NDK="$NDK_DIR"
     . "${DIR}/android_configure_armeabiv7.sh"
-    ./Configure android no-shared --prefix="${SRC_DIR}/openssl/r9d-9-armeabiv7" --openssldir=openssl
-    make all install_sw
+    ./Configure android no-shared --openssldir="${SRC_DIR}/openssl/r9d-9-armeabiv7"
+    make all install_sw || exit 1
 )
+fi
 
 # --------
 # libiconv
@@ -59,8 +141,10 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 # This steps are based on the blog post
 # http://danilogiulianelli.blogspot.com/2012/12/how-to-cross-compile-libiconv-for.html
-
+if [ "${DO_LIBICONV}" == "1" ]
+then
 (
+    rm -rf libiconv
     mkdir libiconv
     cd libiconv
     if [ ! -e "libiconv-1.13.1.tar.gz" ]
@@ -74,32 +158,39 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
     ./configure
     cp -r "$DIR/libiconv/jni" ..
     cd ../jni
-    "${NDK_DIR}/ndk-build"
+    "${NDK_DIR}/ndk-build" || exit 1
     cd ..
     mkdir -p r9d-9-armeabiv7a/include
     mkdir -p r9d-9-armeabiv7a/lib
     cp libiconv-1.13.1/include/iconv.h r9d-9-armeabiv7a/include/
     cp libs/armeabi-v7a/libiconv.so r9d-9-armeabiv7a/lib/
 )
+fi
 
 # -----
 # Boost
 # -----
 # Uses the script from MysticTreeGames
 
+if [ "${DO_BOOST}" == "1" ]
+then
 (
+    rm -rf Boost-for-Android
     git clone https://github.com/MysticTreeGames/Boost-for-Android.git
     cd Boost-for-Android
-    git checkout 8075d96cc9ef42d5c52d233b8ee42cb8421a2818
+    git checkout 1c95d349d5f92c5ac1c24e0ec6985272a3e3883c
     patch -p1 < "$DIR/boost-for-android.patch"
-    ./build-android.sh "${NDK_DIR}"
+    PATH="$PATH:$NDK_DIR" ./build-android.sh --boost=1.55.0 --with-libraries=locale,random,date_time,filesystem,system,thread,chrono "${NDK_DIR}" || exit 1
 )
+fi
 
 # -------------
 # android-cmake
 # -------------
-
-git clone https://github.com/taka-no-me/android-cmake.git
+if [ ! -e android-cmake ]
+then
+    git clone https://github.com/taka-no-me/android-cmake.git
+fi
 
 # ----------
 # casablanca
@@ -111,7 +202,7 @@ git clone https://github.com/taka-no-me/android-cmake.git
     cmake "$DIR/../Release/" \
 	-DCMAKE_TOOLCHAIN_FILE=../android-cmake/android.toolchain.cmake \
 	-DANDROID_ABI=armeabi-v7a \
-	-DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.8 \
+	-DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang3.4 \
 	-DANDROID_STL=none \
 	-DANDROID_STL_FORCE_FEATURES=ON \
         -DANDROID_NATIVE_API_LEVEL=android-9 \
@@ -127,7 +218,7 @@ git clone https://github.com/taka-no-me/android-cmake.git
     cmake "$DIR/../Release/" \
 	-DCMAKE_TOOLCHAIN_FILE=../android-cmake/android.toolchain.cmake \
 	-DANDROID_ABI=armeabi-v7a \
-	-DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.8 \
+	-DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang3.4 \
 	-DANDROID_STL=none \
 	-DANDROID_STL_FORCE_FEATURES=ON \
 	-DANDROID_NDK="${ANDROID_NDK}" \
