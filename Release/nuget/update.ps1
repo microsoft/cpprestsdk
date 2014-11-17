5041c4748ab76cae24502c2081bd03c4978fac5b
Steve Gates <stgates@microsoft.com>
2014-11-17 14:10:57 -0800

    Updating nuget package generation files with some fixes.

--- Release/nuget/update.ps1
+++ Release/nuget/update.ps1
@@ -4,8 +4,8 @@ Param(
 )
 
 Write-Host  "Copying Binaries folder"
-Get-Item ..\..\Binaries\ | Copy-Item -Destination .\Binaries -rescure
-Get-Item ..\include\ | Copy-Item -Destination .\include -rescure
+Get-Item ..\..\Binaries\ | Copy-Item -Destination .\Binaries -recurse
+Get-Item ..\include\ | Copy-Item -Destination .\include -recurse
 
 Write-Host "Updating the cpprestsdk.nuspec version number"
 (Get-Content .\cpprestsdk.nuspec) | ForEach-Object {$_ -replace $OldVer, $NewVer} | Set-Content .\cpprestsdk.nuspec
