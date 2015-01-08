90036c0167d0d43ff6377f23c5c3e70f1c8a0230
Steve Gates <stgates@microsoft.com>
2015-01-08 15:47:43 -0800

    Experimental change to try using Windows Threadpool for ppl tasks.

--- Release/include/pplx/pplxtasks.h
+++ Release/include/pplx/pplxtasks.h
@@ -28,28 +28,28 @@
 #ifndef _PPLXTASKS_H
 #define _PPLXTASKS_H
 
-#if (defined(_MSC_VER) && (_MSC_VER >= 1800))
-#include <ppltasks.h>
-namespace pplx = Concurrency;
-#if (_MSC_VER >= 1900)
-#include <concrt.h>
-namespace Concurrency {
-    namespace extensibility {
-        typedef ::std::condition_variable condition_variable_t;
-        typedef ::std::mutex critical_section_t;
-        typedef ::std::unique_lock< ::std::mutex> scoped_critical_section_t;
-
-        typedef ::Concurrency::event event_t;
-        typedef ::Concurrency::reader_writer_lock reader_writer_lock_t;
-        typedef ::Concurrency::reader_writer_lock::scoped_lock scoped_rw_lock_t;
-        typedef ::Concurrency::reader_writer_lock::scoped_lock_read scoped_read_lock_t;
-
-        typedef ::Concurrency::details::_ReentrantBlockingLock recursive_lock_t;
-        typedef recursive_lock_t::_Scoped_lock scoped_recursive_lock_t;
-    }
-}
-#endif // _MSC_VER >= 1900
-#else
+//#if (defined(_MSC_VER) && (_MSC_VER >= 1800))
+//#include <ppltasks.h>
+//namespace pplx = Concurrency;
+//#if (_MSC_VER >= 1900)
+//#include <concrt.h>
+//namespace Concurrency {
+//    namespace extensibility {
+//        typedef ::std::condition_variable condition_variable_t;
+//        typedef ::std::mutex critical_section_t;
+//        typedef ::std::unique_lock< ::std::mutex> scoped_critical_section_t;
+//
+//        typedef ::Concurrency::event event_t;
+//        typedef ::Concurrency::reader_writer_lock reader_writer_lock_t;
+//        typedef ::Concurrency::reader_writer_lock::scoped_lock scoped_rw_lock_t;
+//        typedef ::Concurrency::reader_writer_lock::scoped_lock_read scoped_read_lock_t;
+//
+//        typedef ::Concurrency::details::_ReentrantBlockingLock recursive_lock_t;
+//        typedef recursive_lock_t::_Scoped_lock scoped_recursive_lock_t;
+//    }
+//}
+//#endif // _MSC_VER >= 1900
+//#else
 
 #include "pplx/pplx.h"
 
@@ -7349,7 +7349,7 @@ namespace details
 #endif
 #pragma pack(pop)
 
-#endif // (defined(_MSC_VER) && (_MSC_VER >= 1800))
+//#endif // (defined(_MSC_VER) && (_MSC_VER >= 1800))
 
 #ifndef _CONCRT_H
 #ifndef _LWRCASE_CNCRRNCY
