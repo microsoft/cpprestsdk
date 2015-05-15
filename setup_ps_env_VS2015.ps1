9c59737a6fd25e1d856cf19023f02a015960c9d3
Steve Gates <stgates@microsoft.com>
2015-05-15 15:38:01 -0700

    Removing unnecessary code from 2015 powershell script.

--- setup_ps_env_VS2015.ps1
+++ setup_ps_env_VS2015.ps1
@@ -1,18 +1,3 @@
-function Get-Batchfile ($file) {
-    $cmd = "@echo off & `"$file`" & set"
-    cmd /c $cmd | Foreach-Object {
-        $p, $v = $_.split('=')
-        Set-Item -path env:$p -value $v
-    }
-}
-
-function VsVars32()
-{
-    $vs140comntools = (Get-ChildItem env:VS140COMNTOOLS).Value
-    $batchFile = [System.IO.Path]::Combine($vs140comntools, "vsvars32.bat")
-    Get-Batchfile $BatchFile
-}
-
 "Initializing C++ REST SDK Powershell VS2015 Environment"
 
 # Add MSBuild to the path.
