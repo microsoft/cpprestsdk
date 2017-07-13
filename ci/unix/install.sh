90f120038de4d8de9f709adeb17e1154f779b645
Hannes Rantzsch <hannes.rantzsch@nexenio.com>
2017-07-13 15:50:54 +0200

    make install, then check if install dir exists

--- ci/unix/install.sh
+++ ci/unix/install.sh
@@ -7,10 +7,10 @@ SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 
 [ -d ${SOURCE_DIR}/ci ] || die "CWD must be repository root"
 [ -d $BUILD_DIR       ] || die "build location '${BUILD_DIR}' doesn't exist"
-[ -d $INSTALL_DIR     ] || die "install location '${INSTALL_DIR}' doesn't exist"
 
 cd $BUILD_DIR
 make install
+[ -d $INSTALL_DIR     ] || die "install location '${INSTALL_DIR}' doesn't exist"
 
 cd $INSTALL_DIR
 tar czvf cpprest.tar.gz *
