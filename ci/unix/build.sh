ec4910bdd00f610517270286ec1bca2a776c5647
René Meusel <rene.meusel@nexenio.com>
2017-07-21 15:31:08 +0200

    don't build sample applications

--- ci/unix/build.sh
+++ ci/unix/build.sh
@@ -8,5 +8,6 @@ cd ${BUILD_DIR}
 
 CC="clang" CXX="clang++"                                                       \
     cmake ../Release -DCMAKE_BUILD_TYPE=Release                                \
+                     -DBUILD_SAMPLES=no                                        \
                      -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/${INSTALL_PREFIX}
 make -j4
