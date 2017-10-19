d305566190b927fe401b509eee9ec71c1f9790d1
Chris Deering <deeringc@gmail.com>
2017-10-19 22:52:16 +0100

    Fix an issue where requests sent via authenticated proxies could not be successfully redirected from one domain to another

--- Release/src/http/client/http_client_winhttp.cpp
+++ Release/src/http/client/http_client_winhttp.cpp
@@ -1038,6 +1038,25 @@ private:
         }
     }
 
+    static utility::string_t get_request_url(HINTERNET hRequestHandle)
+    {
+        DWORD urlSize{ 0 };
+        if(FALSE == WinHttpQueryOption(hRequestHandle, WINHTTP_OPTION_URL, nullptr, &urlSize) || urlSize == 0)
+        {
+            return U("");
+        }
+
+        auto urlwchar = new WCHAR[urlSize / sizeof(WCHAR)];
+
+        WinHttpQueryOption(hRequestHandle, WINHTTP_OPTION_URL, (void*)urlwchar, &urlSize);
+
+        utility::string_t url(urlwchar);
+
+        delete[] urlwchar;
+
+        return url;
+    }
+
     // Returns true if we handle successfully and resending the request
     // or false if we fail to handle.
     static bool handle_authentication_failure(
@@ -1094,10 +1113,22 @@ private:
                 cred = p_request_context->m_http_client->client_config().credentials();
                 p_request_context->m_server_authentication_tried = true;
             }
-            else if (dwAuthTarget == WINHTTP_AUTH_TARGET_PROXY && !p_request_context->m_proxy_authentication_tried)
+            else if (dwAuthTarget == WINHTTP_AUTH_TARGET_PROXY)
             {
-                cred = p_request_context->m_http_client->client_config().proxy().credentials();
-                p_request_context->m_proxy_authentication_tried = true;
+                bool is_redirect = false;
+                try
+                {
+                    web::uri current_uri(get_request_url(hRequestHandle));
+                    is_redirect = p_request_context->m_request.absolute_uri().to_string() != current_uri.to_string();	
+                }
+                catch (const std::exception&) {}
+     
+                // If we have been redirected, then WinHttp needs the proxy credentials again to make the next request leg (which may be on a different server)
+                if (is_redirect || !p_request_context->m_proxy_authentication_tried)
+                {
+                    cred = p_request_context->m_http_client->client_config().proxy().credentials();
+                    p_request_context->m_proxy_authentication_tried = true;
+                }
             }
 
             // No credentials found so can't resend.
