68e8d841ff22d4aab6f603b2f21cdbe8e626a124
Tomas Zemaitis <t.zemaitis@idscan.co.uk>
2018-03-14 11:30:50 +0200

    fixed strand

--- Release/libs/websocketpp/websocketpp/transport/asio/connection.hpp
+++ Release/libs/websocketpp/websocketpp/transport/asio/connection.hpp
@@ -422,7 +422,7 @@ protected:
         m_io_service = io_service;
 
         if (config::enable_multithreading) {
-            m_strand = lib::make_shared<boost::asio::strand>(
+            m_strand = lib::make_shared<boost::asio::io_service::strand>(
                 lib::ref(*io_service));
 
             m_async_read_handler = m_strand->wrap(lib::bind(
