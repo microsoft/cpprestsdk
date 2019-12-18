51be56685c52f2587746a82cf72ef0f76bbf9a41
Billy Robert O'Neal III <bion@microsoft.com>
2019-12-17 16:57:32 -0800

    Remove tests looking for swordsoftruth.com as that host appears gone.

--- Release/tests/functional/websockets/client/authentication_tests.cpp
+++ Release/tests/functional/websockets/client/authentication_tests.cpp
@@ -131,45 +131,10 @@ SUITE(authentication_tests)
 // These tests are specific to our websocketpp based implementation.
 #if !defined(__cplusplus_winrt)
 
-    void sni_test_impl(websocket_client & client)
-    {
-        try
-        {
-            client.connect(U("wss://swordsoftruth.com")).wait();
-
-            // Should never be reached.
-            VERIFY_IS_TRUE(false);
-        }
-        catch (const websocket_exception& e)
-        {
-            if (is_timeout(e.what()))
-            {
-                // Since this test depends on an outside server sometimes it sporadically can fail due to timeouts
-                // especially on our build machines.
-                return;
-            }
-
-            // This test just covers establishing the TLS connection and verifying
-            // the server certificate, expect it to return an unexpected HTTP status code.
-            if (e.error_code().value() == 20)
-            {
-                return;
-            }
-            throw;
-        }
-    }
-
-    // Test specifically for server SignalR team hit interesting cases with.
-    TEST(sni_with_older_server_test)
-    {
-        websocket_client client;
-        sni_test_impl(client);
-    }
-
     // WinRT doesn't expose option for disabling.
     // No stable server is available to reliably test this.
     // The configuration below relies on a timeout in the success case.
-    TEST(disable_sni, "Ignore", "Manual")
+    TEST(disable_sni)
     {
         websocket_client_config config;
         config.set_server_name("expired.badssl.com");
@@ -192,17 +157,6 @@ SUITE(authentication_tests)
             }
             throw;
         }
-    }
-
-    // Winrt doesn't allow explicitly setting server host for SNI.
-    TEST(sni_explicit_hostname)
-    {
-        websocket_client_config config;
-        const auto& name = utf8string("swordsoftruth.com");
-        config.set_server_name(name);
-        VERIFY_ARE_EQUAL(name, config.server_name());
-        websocket_client client(config);
-        sni_test_impl(client);
     }
 
     void handshake_error_test_impl(const ::utility::string_t& host)
