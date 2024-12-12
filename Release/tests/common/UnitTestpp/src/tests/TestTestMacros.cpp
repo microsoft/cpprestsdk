19bdfb18edd2aaa89d85b74f925bf7c0950445dc
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-12-11 16:45:17 -1000

    spelling: successfully
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- Release/tests/common/UnitTestpp/src/tests/TestTestMacros.cpp
+++ Release/tests/common/UnitTestpp/src/tests/TestTestMacros.cpp
@@ -179,7 +179,7 @@ TEST(CorrectlyReportsFixturesWithCtorsThatAssert)
 } // namespace
 
 // We're really testing if it's possible to use the same suite in two files
-// to compile and link successfuly (TestTestSuite.cpp has suite with the same name)
+// to compile and link successfully (TestTestSuite.cpp has suite with the same name)
 // Note: we are outside of the anonymous namespace
 SUITE(SameTestSuite)
 {
